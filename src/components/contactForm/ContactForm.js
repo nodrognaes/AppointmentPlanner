9a0536bcc379e972f8f3822d88231f6d0ad7c72b
Sean Gordon <nodrog.naes@gmail.com>
2022-01-24 13:26:56 -0500

    contact form render

--- src/components/contactForm/ContactForm.js
+++ src/components/contactForm/ContactForm.js
@@ -10,6 +10,32 @@ export const ContactForm = ({
   handleSubmit
 }) => {
   return (
-    ContactForm
+    <form onSubmit={handleSubmit}>
+      <input 
+        type="text"
+        value={name}
+        onChange={(e) => {
+          setName(e.target.value)
+        }}
+        required
+      />
+      <input
+        type="tel"
+        value={phone}
+        onChange={(e) => {
+          setPhone(e.target.value)
+        }}
+        required 
+      />
+      <input 
+        type="email"
+        value={email}
+        onChange={(e) => {
+          setEmail(e.target.value)
+        }}
+        required
+      />
+      <input type="submit" />
+    </form>
   );
 };
